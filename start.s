21a85071bd969edcba87127e434b48c1af743eeb
Zachary Yedidia <zyedidia@gmail.com>
2022-04-28 21:53:22 -0700

    Add start.s

---
+++ start.s
@@ -0,0 +1,20 @@
+#include "asm.h"
+
+.section ".text.boot"
+
+.globl _start
+_start:
+	# force the mode to be super
+	mov r0, #SUPER_MODE
+	# disable interrupts
+	orr r0, r0, #(1 << 7)
+	msr cpsr, r0
+	prefetch_flush(r1)
+
+	mov sp, #STACK_ADDR
+	# clear frame pointer
+	mov fp, #0
+	bl cstart
+hlt:
+	b hlt
+
