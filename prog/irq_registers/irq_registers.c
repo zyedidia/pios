120cea5d845ba8d484b87945886570697f60d87c
Matthew Sotoudeh <matthewsot@outlook.com>
2022-05-07 16:33:39 -0700

    time slicing

--- prog/irq_registers/irq_registers.c
+++ prog/irq_registers/irq_registers.c
@@ -1,52 +1,76 @@
 #include "pios.h"
 #include "dev.h"
 
-volatile unsigned count = 0;
+extern void irq_vec_asm();
+
+struct thread {
+    volatile uint32_t registers[16];
+};
+
+volatile struct thread threads[100];
+volatile unsigned n_threads = 1;
+volatile unsigned curr_thread_id = 0;
+
+void swippityswap(volatile uint32_t *live_state, volatile struct thread *new_thread, volatile struct thread *old_thread) {
+    for (size_t i = 0; i < 16; i++) {
+        old_thread->registers[i] = live_state[i];
+        live_state[i] = new_thread->registers[i];
+    }
+}
 
 void irq(uint32_t *state_ptr) {
     dev_barrier();
     if (!timer_has_irq()) return;
 
-    if (count++ > 3) reboot();
+    unsigned next_thread_id = (curr_thread_id + 1) % n_threads;
+    if (next_thread_id == curr_thread_id) goto end;
 
-    for (size_t i = 0; i < 16; i++) {
-        printf("Register %u is %lu\n", i, state_ptr[i]);
-    }
+    swippityswap(state_ptr, &(threads[next_thread_id]), &(threads[curr_thread_id]));
+    curr_thread_id = next_thread_id;
 
+end:
     dev_barrier();
     timer_clear_irq();
     dev_barrier();
 }
 
-extern void irq_vec_asm();
+void spawn(void (*foo)(unsigned), unsigned arg) {
+    uint32_t *stack = kmalloc(1024 * 1024 * 4);
+    unsigned thread_id = n_threads++;
+
+    for (size_t i = 0; i < 16; i++)
+        threads[thread_id].registers[i] = 0;
+
+    threads[thread_id].registers[0] = arg;
+    threads[thread_id].registers[13] = (uint32_t)stack;
+    threads[thread_id].registers[15] = (uint32_t)foo;
+}
+
+void worker(unsigned which) {
+    bool lit = false;
+    while (true) {
+        gpio_write(which, lit);
+        lit = !lit;
+        printf("Hello from worker on gpio %u\n", which);
+        delay_ms(1000);
+    }
+}
 
 int main() {
     gpio_set_output(21);
+    gpio_set_output(20);
 
     irq_init_table();
     irq_enable(BASIC_TIMER_IRQ);
-    timer_irq_load(0x400);
+    timer_irq_load(0x1000);
     register_irq_vec(IRQ_VEC_IRQ, (uintptr_t) irq_vec_asm);
 
     enable_interrupts();
 
-    while (true) {
-        asm ("mov r0,  $0");
-        asm ("mov r1,  $1");
-        asm ("mov r2,  $2");
-        asm ("mov r3,  $3");
-        asm ("mov r4,  $4");
-        asm ("mov r5,  $5");
-        asm ("mov r6,  $6");
-        asm ("mov r7,  $7");
-        asm ("mov r8,  $8");
-        asm ("mov r9,  $9");
-        asm ("mov r10, $10");
-        asm ("mov r11, $11");
-        asm ("mov r12, $12");
-        asm ("mov r13, $13");
-        asm ("mov r14, $14");
-    }
-    delay_ms(3000);
+    spawn(worker, 21);
+    spawn(worker, 20);
+
+    while (true);
+
     return 0;
 }
