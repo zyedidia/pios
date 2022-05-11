98a866ccc4405f2410fdba6c079e0f47a19a83e2
Matthew Sotoudeh <matthewsot@outlook.com>
2022-05-10 19:14:58 -0700

    threads

---
+++ prog/threads/irq_asm.s
@@ -0,0 +1,18 @@
+#define INT_STACK_ADDR 0x9000000
+
+.globl irq_vec_asm
+// TODO: Need to also save cpsr, maybe other state ?
+irq_vec_asm:
+    mov sp, #INT_STACK_ADDR
+    sub lr, lr, #4              // Update the lr to where we should jump back to
+
+    push {lr}
+    stmfd sp, {r0-r14}^
+    sub sp, sp, #60
+    mov r0, sp
+
+    bl irq                      // call our actual irq handler
+
+    ldm sp, {r0-r15}^           // restore all registers, jump back.
+                                // including r15 also tells ldm to restore spsr
+                                // into cpsr
