bdaf6377c3edf65132875fd092dd393619dcfb3a
Zachary Yedidia <zyedidia@gmail.com>
2022-05-12 19:36:54 -0700

    Address sanitizer

--- kern/kmalloc.h
+++ kern/kmalloc.h
@@ -3,10 +3,17 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "kern.h"
+
 void kmalloc_init();
 void* kmalloc(size_t size);
 void* kmalloc_aligned(size_t size, size_t align);
 void kfree(void* p);
 
-uintptr_t heap_start();
-uintptr_t heap_end();
+static inline uintptr_t pagenum(uintptr_t pa) {
+    return pa / PAGESIZE;
+}
+
+static inline uintptr_t pageaddr(uintptr_t pn) {
+    return pn * PAGESIZE;
+}
